aee3d0344f351866fefcd927bdd02a2f8575ea9b
aray-1 <humanrace.450@gmail.com>
2024-04-05 00:42:13 +0530

    MQTT Client for raspi 5

--- esp8266_HCSR04_client/src/main.cpp
+++ esp8266_HCSR04_client/src/main.cpp
@@ -3,21 +3,21 @@
 #include <PubSubClient.h>
 
 // WiFi credentials
-const char* ssid = "raspberrypi_4b";//raspberrypi_4b
-const char* password = "raspi_4b"; //raspi_4b
-const char* MQTT_username = "vsh_pi4b"; //vsh_pi4b
-const char* MQTT_password = "12345678"; //12345678
+const char *ssid = "raspberrypi_5";
+const char *password = "12345678";
+// const char *MQTT_username = "vsh_pi5";
+// const char *MQTT_password = "12345678";
 
 // MQTT Broker
-const char* mqtt_server = "10.42.0.1"; //10.42.0.1
-const int mqtt_port = 1883; // Default MQTT port
+const char *mqtt_server = "10.42.0.1";
+const int mqtt_port = 1883;
 
 // MQTT Client
 WiFiClient espClient;
 PubSubClient client(espClient);
 
 // Topic to which ESP8266 will publish
-const char* channel = "/esp/obstacle";
+const char *channel = "/esp/obstacle";
 
 // Ultrasonic sensor
 #define TRIGGER_PIN 12
@@ -25,63 +25,68 @@ const char* channel = "/esp/obstacle";
 #define SOUND_VELOCITY 0.034 // define sound velocity in cm/uS
 #define CM_TO_INCH 0.393701
 
+#define UNIT_LOCATION 0 // up or down
+#define UNIT_POSITION 0 // left or right
+
 long duration;
 float distanceCm;
 float distanceInch;
 
 // Function to reconnect to MQTT broker
-void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    if (client.connect("ESP8266Client",MQTT_username,MQTT_password)) {
-      Serial.println("connected");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
+void reconnect(){
+	// Loop until we're reconnected
+	while (!client.connected()){
+		Serial.print("Attempting MQTT connection...");
+		// Attempt to connect
+		if (client.connect("ESP8266Client")){
+			Serial.println("connected");
+		}
+		else{
+			Serial.print("failed, rc=");
+			Serial.print(client.state());
+			Serial.println(" try again in 5 seconds");
+			// Wait 5 seconds before retrying
+			delay(5000);
+		}
+	}
 }
 
-void setup() {
-  Serial.begin(115200); // Starts the serial communication
-  // Connect to Wi-Fi
-  WiFi.begin(ssid, password);
-  Serial.println();
+void setup(){
+	Serial.begin(115200); // Starts the serial communication
+	// Connect to Wi-Fi
+	WiFi.begin(ssid, password);
+	Serial.println();
 
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print("Wifi not connected! Retrying...");
-  }
+	// Wait for connection
+	while (WiFi.status() != WL_CONNECTED){
+		delay(500);
+		Serial.print("Wifi not connected! Retrying...");
+	}
 
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+	Serial.println("");
+	Serial.println("WiFi connected");
+	Serial.println("IP address: ");
+	Serial.println(WiFi.localIP());
 
-  // Setup MQTT client
-  client.setServer(mqtt_server, mqtt_port);
+	// Setup MQTT client
+	client.setServer(mqtt_server, mqtt_port);
 
-  // Setup ultrasonic sensor
-  pinMode(TRIGGER_PIN, OUTPUT); // Sets the TRIGGER_PIN as an Output
-	pinMode(ECHO_PIN, INPUT);  // Sets the ECHO_PIN as an Input
+	// Setup ultrasonic sensor
+	pinMode(TRIGGER_PIN, OUTPUT); // Sets the TRIGGER_PIN as an Output
+	pinMode(ECHO_PIN, INPUT);	  // Sets the ECHO_PIN as an Input
 }
 
-void loop() {
-  if (!client.connected()) {
-    reconnect();
-  }
-  
-  // Sensing distance using ultrasonic sensor
-  // Clears the TRIGGER_PIN
+void loop()
+{
+	if (!client.connected()){
+		reconnect();
+	}
+
+	// Sensing distance using ultrasonic sensor
+	// Clears the TRIGGER_PIN
 	digitalWrite(TRIGGER_PIN, LOW);
 	delayMicroseconds(2);
-	
+
 	// Sets the TRIGGER_PIN on HIGH state for 10 micro seconds
 	digitalWrite(TRIGGER_PIN, HIGH);
 	delayMicroseconds(10);
@@ -97,17 +102,19 @@ void loop() {
 	Serial.print("Distance (cm): ");
 	Serial.println(distanceCm);
 
-  // Polling to check threshold
-  bool threshold = false;
-  if(distanceCm < 15)
-  threshold = true; // Send interrupt if distance is less than 3inch
-  else threshold = false;
+	// Polling to check threshold
+	bool threshold = false;
 
-  // Send data
-  String payload = String(threshold);
-  //String payload2 = String(warning);
-  client.publish(channel, payload.c_str());
-  //client.publish(channel, payload2.c_str());
+	if (distanceCm < 15) threshold = true; // Send interrupt if distance is less than 3inch
+	else threshold = false;
 
-  delay(1000); // Publish data every second
+	// Generate payload
+	String payload;
+
+	payload = "{\"th\":" + String(threshold) + ",\"loc\":" + String(UNIT_LOCATION) + ",\"pos\":" + String(UNIT_POSITION) + "}";
+
+	// Send data
+	if(threshold) client.publish(channel, payload.c_str());
+	
+	delay(100); // Publish data every second
 }
